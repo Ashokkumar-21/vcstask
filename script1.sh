e0f379d40b78011bac289090fedd0943b5dcae3e
Ashokkumar-21 <ashoksulthan21@gmail.com>
2025-06-15 11:30:53 +0000

    main branch script1 edit

--- script1.sh
+++ script1.sh
@@ -2,4 +2,8 @@
 
 echo "This is Script file 1"
 
+<<<<<<< HEAD
 echo "This is devops branch script file edited, now add another change"
+=======
+echo "This is devops branch script file edited and this line is add from main"
+>>>>>>> a4c4956 (main branch script1 edit)
